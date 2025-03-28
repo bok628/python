505178f8d39bfffd9bd0f8a632c58645a77048cc
bok628 <bokyung0249@gmail.com>
2025-03-28 12:55:05 +0900

    for 반복문 + if

---
+++ py0328/p0328_04.py
@@ -0,0 +1,31 @@
+# 1,3,5,7,9, ... ,99 1-100사이의 홀수만 숫자를 더해서 합계 구하기
+sum = 0
+i = 0
+for i in range(1,100,2) :
+    sum = sum + i
+print("합계 : ",sum)
+
+# 3의 배수만 더하기
+sum = 0
+i = 0
+for i in range(1,101) :
+    if i % 3 == 0 :
+        sum = sum + i
+        print("i : {}, sum : {}".format(i,sum))
+print("합계 : ",sum)
+
+# 3의 배수이면서 5의 배수만 더하기
+sum = 0
+i = 0
+for i in range(1,101) :
+    if i % 3 == 0 and i % 5 == 0 :
+        sum = sum + i
+        print("i : {} sum : {}".format(i,sum))
+print("합계 : ",sum)
+    
+sum = 0
+i = 0
+for i in range(1,101) :
+    if i % 3 == 0 or i % 7 == 0 :
+        sum = sum + i
+        print(" i : {}, sum : {}".format(i,sum))
